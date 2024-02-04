2d8e1931d94ed397e0348b37a741d8f12470a225
Minati Alphonso <61948095+MinatiAlphonso@users.noreply.github.com>
2024-02-03 22:21:28 -0700

    modified gitignore

--- ElderEase/ElderEase/wwwroot/js/site.js
+++ ElderEase/ElderEase/wwwroot/js/site.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var providerPortalDiv = document.getElementById('providerPortal');
 
     // Adding click event listener
-    // Redirecting to SignIn.cshtml
+    // Redirecting to SignIn.cshtml with a query string
     clientPortalDiv.addEventListener('click', function() {
         window.location.href = '/SignIn?Role=client';
     });
